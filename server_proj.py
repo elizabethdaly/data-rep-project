3295fd2f00b5eafd343d7255452897eedfec1b9e
elizabethdaly <liz.m.a.daly@gmail.com>
2019-11-26 18:20:40 +0000

    Do basic crud

--- server_proj.py
+++ server_proj.py
@@ -5,6 +5,7 @@ from flask import Flask, jsonify, request, abort
 
 app = Flask(__name__, static_url_path='', static_folder='.')
 
+##################################################################
 # List of foods (or a DB)
 # Price is a float .xx
 foods=[
@@ -15,16 +16,18 @@ foods=[
 
 # Keep track of ID as need to increment for new food
 # This is a global var
-nextId = 3
+nextId = 4
 
-# Maybe have a shopping basket later?
-basket=[
+##################################################################
+# Maybe have another array for second DB like a cart?
+cart=[
     {"id": 1, "name":"chedder", "amount": 2},
     {"id": 2, "name":"carrots", "amount:": 0},
     {"id": 3, "name":"milk", "amount:": 1}
 ]
 # Keep track of ID as need to increment for new food in basket
-basketId = 4
+cartId = 4
+##################################################################
 
 # Action = Get all
 # curl "http://127.0.0.1:5000/foods"
@@ -33,6 +36,72 @@ def getAll():
     #return "in getAll"
 
     return jsonify(foods)
+
+##################################################################
+
+# Action = Find food by ID
+# curl "http://127.0.0.1:5000/foods/1"
+@app.route('/foods/<int:id>')
+def findByID(id):
+    #return "in find by ID for id "+ str(id)
+
+    #for each f in foods, check id until match
+    foundFoods = list(filter(lambda f: f['id'] == id, foods))
+    
+    # Maybe print error message?
+    if len(foundFoods) == 0:
+        return jsonify({}), 204
+
+    # Else return the food by requested id
+    return jsonify(foundFoods[0])
+
+##################################################################
+
+# Action = Create a new food
+# curl -i -H "Content-Type:application/json" -X POST -d "{\"name\":\"wine\", \"price\": 2.50 }" "http://127.0.0.1:5000/foods"
+@app.route('/foods', methods=['POST'])
+def create():
+    global nextId # To allow edits inside this function
+    #return "in create"
+
+    if not request.json:
+        abort(400)
+
+    # Do other checking for more marks eg proper format?
+    # price must be a float
+
+    # id increments automatically
+    # Suppy name and price in request
+    food = {
+        "id": nextId,
+        "name": request.json['name'],
+        "price": request.json['price']
+    }
+
+    nextId += 1
+    foods.append(food)
+    return jsonify(food)
+
+##################################################################
+
+# Action = Delete by ID
+# curl -X DELETE "http://127.0.0.1:5000/foods/1"
+@app.route('/foods/<int:id>', methods=['DELETE'])
+def delete(id):
+    #return "in delete by ID for id "+ str(id)
+
+    #for each f in foods, check id until match
+    foundFoods = list(filter(lambda f: f['id'] == id, foods))
+    
+    # Maybe print error message?
+    if (len(foundFoods) == 0):
+        abort(404)
+
+    # Else remove the food by requested id
+    foods.remove(foundFoods[0])
+    return jsonify({"done":True})
+
+##################################################################
 
 if __name__ == '__main__' :
     app.run(debug= True)
